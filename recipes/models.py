ff10e9eaadd8d7a7c1d3bd6ae560b299b9c5292a
Arturitooo <arturito.python@gmail.com>
2023-11-09 20:19:14 +0100

    fixing recipes

--- recipes/models.py
+++ recipes/models.py
@@ -1,27 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-CUISINE_COUNTRIES={
-    'Polish':'🇵🇱',
-    "German":"🇩🇪",
-    "Italian":"🇮🇹",
-    "French":"🇫🇷",
-    "American":"🇺🇸",
-    "Spanish":"🇪🇸",
-    "Japanese":"🇯🇵",
-    "Indian":"🇮🇳",
-    "Other":"",
-    }
+CUISINE_COUNTRIES = [
+    ("Polish", "🇵🇱"),
+    ("German", "🇩🇪"),
+    ("Italian", "🇮🇹"),
+    ("French", "🇫🇷"),
+    ("American", "🇺🇸"),
+    ("Spanish", "🇪🇸"),
+    ("Japanese", "🇯🇵"),
+    ("Indian", "🇮🇳"),
+    ("Other", ""),
+]
+
 
 # Create your models here.
 class Recipe(models.Model):
     recipe_author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     recipe_name = models.CharField(max_length=250)
-    time_consumption = models.IntegerField() #hours - 1,5 should give user 1h30min
+    time_consumption = models.IntegerField()  # hours - 1,5 should give user 1h30min
     cuisine = models.CharField(choices=CUISINE_COUNTRIES, max_length=25)
-    recipe_image = models.ImageField(upload_to='uploads/recipes')
+    recipe_image = models.ImageField(upload_to="uploads/recipes")
     guidelines = models.CharField(max_length=5000)
 
-
     def __str__(self):
-        return f"{self.user} - {self.recipe_name} - {self.pk}"
+        return f"{self.recipe_author} - {self.recipe_name} - {self.pk}"
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-def mainpage(request):
-    #search
-    #list of recipes
-    #recipe details page
-    #add to favourite
-    #comment recipe
-    #rate recipe
+def recipes(request):
+    # search
+    # list of recipes
+    # recipe details page
+    # add to favourite
+    # comment recipe
+    # rate recipe
     return render(request, "recipes/index.html")
