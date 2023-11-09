e5a931a6f1a9714fbc90aef7178a026bd3af0b21
Arturitooo <arturito.python@gmail.com>
2023-11-09 22:28:45 +0100

    middle way of finishing adding recipe form

--- recipes/models.py
+++ recipes/models.py
@@ -13,15 +13,40 @@ CUISINE_COUNTRIES = [
     ("Other", ""),
 ]
 
+MEALS = [
+    ("Breafkfest", "Breafkfest"),
+    ("Brunch", "Brunch"),
+    ("Dinner", "Dinner"),
+    ("Supper", "Supper"),
+]
+
 
 # Create your models here.
 class Recipe(models.Model):
     recipe_author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     recipe_name = models.CharField(max_length=250)
     time_consumption = models.IntegerField()  # hours - 1,5 should give user 1h30min
-    cuisine = models.CharField(choices=CUISINE_COUNTRIES, max_length=25)
+    cuisine = models.CharField(choices=CUISINE_COUNTRIES, max_length=50)
+    meal_type = models.CharField(choices=MEALS, max_length=50)
     recipe_image = models.ImageField(upload_to="uploads/recipes")
-    guidelines = models.CharField(max_length=5000)
 
     def __str__(self):
         return f"{self.recipe_author} - {self.recipe_name} - {self.pk}"
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    amount = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.recipe} - {self.name}"
+
+
+class Instruction(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    step_number = models.CharField(max_length=50)
+    description = models.CharField(max_length=1500)
+
+    def __str__(self):
+        return f"{self.recipe} - {self.step_number}"
