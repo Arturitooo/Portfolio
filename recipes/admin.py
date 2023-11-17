58723d9bc59d389d402943cc1980799ee1c80921
Arturitooo <arturito.python@gmail.com>
2023-11-17 17:28:28 +0100

    adding models to admin in recipes, first unit test added in recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Favorite_recipe, Ingredient, Instruction, RecipeRating
 
 # Register your models here.
 admin.site.register(Recipe)
+admin.site.register(Favorite_recipe)
+admin.site.register(Ingredient)
+admin.site.register(Instruction)
+admin.site.register(RecipeRating)
--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Recipe, Favorite_recipe, Ingredient, Instruction, RecipeRating
 
 # Create your tests here.
+
+class RecipeModelTest(TestCase):
+    def setUp(self):
+        # Create a user for testing
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_recipe_creation(self):
+        # Test creating a Recipe instance
+        recipe = Recipe.objects.create(
+            recipe_author=self.user,
+            recipe_name='Test Recipe',
+            time=30,
+            cuisine='Italian',
+            meal_type='Dinner',
+            recipe_image=f'uploads\recipes\image_11.png',
+            average_rating=0,
+            total_ratings=0,
+        )
+        self.assertEqual(recipe.recipe_name, 'Test Recipe')
+        self.assertEqual(recipe.recipe_author, self.user)
+        self.assertEqual(recipe.meal_type, 'Dinner')
+        self.assertEqual(recipe.cuisine, 'Italian')
+        self.assertEqual(recipe.time, 30)
+
+'''    def test_favorite_recipe_creation(self):
+        # Test creating a Favorite_recipe instance
+        favorite_recipe = Favorite_recipe.objects.create(user=self.user, recipe=self.recipe)
+        self.assertEqual(favorite_recipe.user, self.user)
+        self.assertEqual(favorite_recipe.recipe, self.recipe)'''
